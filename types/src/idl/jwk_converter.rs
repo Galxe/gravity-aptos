e582e8b2524a503ec419853862c4c0e1d66d1fef
ByteYue <yj976240184@gmail.com>
2025-08-19 14:08:11 +0800

    oidc provider

--- types/src/idl/jwk_converter.rs
+++ types/src/idl/jwk_converter.rs
@@ -54,11 +54,15 @@ pub fn construct_observed_jwks(bytes: &[u8]) -> Result<ObservedJWKs, JwkIdlError
     Ok(convert_observed_jwks(observed_jwks))
 }
 
-pub fn construct_oidc_provider(bytes: &[u8]) -> Result<OIDCProvider, JwkIdlError> {
-    let oidc_provider = bcs::from_bytes::<api_types::on_chain_config::jwks::OIDCProvider>(bytes)
-        .map_err(|e| JwkIdlError::JsonDeserializationError(e.to_string()))?;
-    Ok(OIDCProvider {
-        name: oidc_provider.name,
-        config_url: oidc_provider.config_url,
-    })
+pub fn construct_oidc_providers(bytes: &[u8]) -> Result<Vec<OIDCProvider>, JwkIdlError> {
+    let oidc_providers =
+        bcs::from_bytes::<Vec<api_types::on_chain_config::jwks::OIDCProvider>>(bytes)
+            .map_err(|e| JwkIdlError::JsonDeserializationError(e.to_string()))?;
+    Ok(oidc_providers
+        .iter()
+        .map(|oidc_provider| OIDCProvider {
+            name: oidc_provider.name.clone(),
+            config_url: oidc_provider.config_url.clone(),
+        })
+        .collect())
 }
