0d91ac7b13626e07fed722cd283fe2f0bcad15a5
Lightman <915311741@qq.com>
2025-09-10 09:55:07 +0800

    tmp

--- crates/api-types/src/relayer.rs
+++ crates/api-types/src/relayer.rs
@@ -6,7 +6,7 @@ use std::sync::{Arc, OnceLock};
 pub trait Relayer: Send + Sync + 'static {
     async fn add_uri(&self, uri: &str, rpc_url: &str) -> Result<(), ExecError>;
 
-    async fn get_last_state(&self, uri: &str) -> Result<JWKStruct, ExecError>;
+    async fn get_last_state(&self, uri: &str) -> Result<Vec<JWKStruct>, ExecError>;
 }
 
 pub static GLOBAL_RELAYER: OnceLock<Arc<dyn Relayer>> = OnceLock::new();
