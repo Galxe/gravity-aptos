7459a8ea954c7ccfcb43c008b2c54c9d066c5ba2
Lightman <915311741@qq.com>
2025-10-11 15:38:17 +0800

    tmp

--- crates/api-types/src/config_storage.rs
+++ crates/api-types/src/config_storage.rs
@@ -43,6 +43,7 @@ impl FromStr for OnChainConfig {
             "CurrentTimeMicroseconds" => Ok(OnChainConfig::CurrentTimeMicroseconds),
             "ObservedJWKs" => Ok(OnChainConfig::ObservedJWKs),
             "Features" => Ok(OnChainConfig::Features),
+            "DKGState" => Ok(OnChainConfig::DKGState),
             _ => Err(format!("Unknown OnChainConfig variant: {}", s)),
         }
     }
