8b6f62149bf161d3f5982730c26d2ffeed5aaab3
Lightman <915311741@qq.com>
2025-11-19 19:35:50 +0800

    fix: correct FunctionValueExtension trait bound in event.rs
    - Fix trait bound by dereferencing function_value_extension before passing to with_func_args_deserialization

--- aptos-move/framework/src/natives/event.rs
+++ aptos-move/framework/src/natives/event.rs
@@ -97,7 +97,7 @@ fn native_write_to_event_store(
     let max_value_nest_depth = context.max_value_nest_depth();
     let blob = ValueSerDeContext::new(max_value_nest_depth)
         .with_delayed_fields_serde()
-        .with_func_args_deserialization(function_value_extension)
+        .with_func_args_deserialization(&*function_value_extension)
         .serialize(&msg, &layout)?
         .ok_or_else(|| {
             SafeNativeError::InvariantViolation(PartialVMError::new(
@@ -164,7 +164,7 @@ fn native_emitted_events_by_handle(
             let function_value_extension = context.function_value_extension();
             let max_value_nest_depth = context.max_value_nest_depth();
             ValueSerDeContext::new(max_value_nest_depth)
-                .with_func_args_deserialization(function_value_extension)
+                .with_func_args_deserialization(&*function_value_extension)
                 .deserialize(blob, &ty_layout)
                 .ok_or_else(|| {
                     SafeNativeError::InvariantViolation(PartialVMError::new(
@@ -198,7 +198,7 @@ fn native_emitted_events(
             let function_value_extension = context.function_value_extension();
             let max_value_nest_depth = context.max_value_nest_depth();
             ValueSerDeContext::new(max_value_nest_depth)
-                .with_func_args_deserialization(function_value_extension)
+                .with_func_args_deserialization(&*function_value_extension)
                 .with_delayed_fields_serde()
                 .deserialize(blob, &ty_layout)
                 .ok_or_else(|| {
@@ -268,7 +268,7 @@ fn native_write_module_event_to_store(
     let max_value_nest_depth = context.max_value_nest_depth();
     let blob = ValueSerDeContext::new(max_value_nest_depth)
         .with_delayed_fields_serde()
-        .with_func_args_deserialization(function_value_extension)
+        .with_func_args_deserialization(&*function_value_extension)
         .serialize(&msg, &layout)?
         .ok_or_else(|| {
             SafeNativeError::InvariantViolation(PartialVMError::new_invariant_violation(
