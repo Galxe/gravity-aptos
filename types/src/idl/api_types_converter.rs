06873d2f5f9dba943d300509d7913031667bad6c
Lightman <915311741@qq.com>
2025-12-21 13:38:37 +0800

    Add reth_account_address field to api-types ValidatorInfo
    Add a new field `reth_account_address: Vec<u8>` to the api-types
    ValidatorInfo struct to support storing Reth account addresses for validators.
    
    Changes:
    - Add `pub reth_account_address: Vec<u8>` field to api-types ValidatorInfo struct
    - Update `new()` method to accept reth_account_address parameter
    - Update `convert_to_api_validator_info()` to include reth_account_address field
    - Update `convert_validator_info()` to read reth_account_address from api-types
    
    This change maintains consistency with the main ValidatorInfo struct in types crate.

--- types/src/idl/api_types_converter.rs
+++ types/src/idl/api_types_converter.rs
@@ -15,7 +15,7 @@ use crate::{
     validator_config::ValidatorConfig,
     validator_info::ValidatorInfo,
 };
-use anyhow::{anyhow, format_err};
+use anyhow::format_err;
 use aptos_crypto::bls12381;
 use bytes::Bytes;
 use std::{convert::TryFrom, str::FromStr};
@@ -95,7 +95,7 @@ pub fn convert_validator_info(
         account_address,
         api_validator_info.consensus_voting_power,
         config,
-        vec![],
+        api_validator_info.reth_account_address.clone(),
     ))
 }
 
@@ -165,6 +165,7 @@ pub fn convert_to_api_validator_info(
         account_address: convert_to_api_account_address(validator_info.account_address()),
         consensus_voting_power: validator_info.consensus_voting_power(),
         config: convert_to_api_validator_config(validator_info.config()),
+        reth_account_address: validator_info.reth_account_address.clone(),
     }
 }
 
