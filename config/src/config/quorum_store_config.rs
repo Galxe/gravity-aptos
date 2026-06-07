0dae78e711ffbbb097dba68f782d27e045be08a3
AshinGau <AshinGau@outlook.com>
2026-06-07 06:42:14 +0000

    revert: proof_timeout back to 10s

--- config/src/config/quorum_store_config.rs
+++ config/src/config/quorum_store_config.rs
@@ -105,7 +105,7 @@ impl Default for QuorumStoreConfig {
     fn default() -> QuorumStoreConfig {
         QuorumStoreConfig {
             channel_size: 1000,
-            proof_timeout_ms: 30000,
+            proof_timeout_ms: 10000,
             batch_generation_poll_interval_ms: 25,
             batch_generation_min_non_empty_interval_ms: 100,
             batch_generation_max_interval_ms: 250,
